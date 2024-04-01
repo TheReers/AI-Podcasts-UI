4249c1aedf1e4814eb421dcde462274e7b2c82c7
Babatunde Mudathir Koiki <38375959+Babatunde13@users.noreply.github.com>
2024-04-01 16:19:34 +0100

    move all env vars to a single file (#4)

---
+++ src/app/api/envs.ts
@@ -0,0 +1,17 @@
+/**
+ * This file is used to store all the environment variables that are used in the application.
+ * It serves as a single source of truth for all the environment variables.
+ * As a result, calling process.env.* outside of this file is discouraged.
+ * All environment variables are prefixed with NEXT_ as suggested by Next.js.
+ */
+const envs = {
+    openAI: {
+        apiKey: process.env.NEXT_OPEN_AI_API_KEY || ''
+    },
+    elevenLabs: {
+        apiKey: process.env.NEXT_ELEVEN_LABS_API_KEY || ''
+    },
+    secretKey: process.env.NEXT_SECRET_KEY || ''
+}
+
+export default envs
