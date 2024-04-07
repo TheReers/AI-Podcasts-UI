ad66b4cb24acc928ae5333e0aaa6f5db1937fc45
Adenikym <65138512+Adenikym@users.noreply.github.com>
2024-04-07 13:33:50 +0100

    chore: axios setup

---
+++ src/app/service/interceptors.ts
@@ -0,0 +1,42 @@
+import {
+    AxiosError,
+    AxiosInstance,
+    AxiosResponse,
+    AxiosRequestConfig,
+  } from "axios";
+  
+  import { getSession } from "next-auth/react";
+  
+  const onRequest = async (
+    config: AxiosRequestConfig
+  ): Promise<AxiosRequestConfig> => {
+    const session = await getSession();
+    const token = (session?.user as any)?.token;
+    config.headers = {
+      ...(config?.headers ?? {}),
+      Authorization: `Bearer ${token}`,
+    };
+    return config;
+  };
+  
+  const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  };
+  
+  const onResponse = (response: AxiosResponse): AxiosResponse => {
+    return response;
+  };
+  
+  const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  };
+  
+  export function setupInterceptorsTo(
+    axiosInstance: AxiosInstance
+  ): AxiosInstance {
+    // @ts-ignore
+    axiosInstance.interceptors.request.use(onRequest, onRequestError);
+    axiosInstance.interceptors.response.use(onResponse, onResponseError);
+    return axiosInstance;
+  }
+  
