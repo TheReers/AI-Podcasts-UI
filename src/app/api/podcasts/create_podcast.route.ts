3993e135f5622cf8952ce0ec2e45bcb0e4361404
Babatunde Koiki <koikibabatunde14@gmail.com>
2024-04-10 15:21:56 +0100

    (feature): save created podcast to cloudinary

---
+++ src/app/api/podcasts/create_podcast.route.ts
@@ -0,0 +1,88 @@
+import getMP3Duration from 'get-mp3-duration'
+import AI from '../utils/ai_generation.util'
+import { Handler } from '../middlewares/types'
+import { uploadFileToCloudinary } from '../utils/save_file.util'
+import podcastModel, { IPodcast, createNewPodcast } from '../db/models/podcast.model'
+
+const duplicatePodcast = async (podcast: IPodcast, user: string) => {
+    const createPodcast = await createNewPodcast({
+        name: podcast.name,
+        slug: podcast.slug,
+        duration: podcast.duration,
+        url: podcast.url,
+        user
+    })
+
+    return createPodcast
+}
+
+export const createPodcast: Handler = async (req) => {
+  const { user } = req
+  if (!user) {
+    return Response.json({ message: 'Unauthorized' }, { status: 401 })
+  }
+
+  const { message } = await req.json()
+  if (!message) {
+    return Response.json({ error: 'message is required' }, { status: 400 })
+  }
+
+  const slug = message.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-').replace(/-+/g, '-').replace(/^-|-$/g, '')
+  const podcastExist = await podcastModel.findOne({ slug })
+  if (podcastExist) {
+    if (podcastExist.user.toString() !== user._id.toString()) {
+        const duplicatePodcastResult = await duplicatePodcast(podcastExist, user._id.toString())
+
+        if (duplicatePodcastResult.error) {
+            return Response.json(duplicatePodcastResult, { status: 400 })
+        }
+
+        return Response.json({
+            message: 'Podcast created successfully',
+            data: duplicatePodcastResult.data.toJSON()
+        }, { status: 201 })
+    }
+
+
+    return Response.json({ message: 'Podcast already exist', data: podcastExist.toJSON() }, { status: 409 })
+  }
+
+  Response.json({ message: 'Creating podcast', data: { slug } }, { status: 201 })
+
+  const ai = new AI()
+  const messageResponse = await ai.generatePodcastText(message)
+  if (messageResponse.error || !messageResponse.data) {
+    return Response.json(messageResponse, { status: 400 })
+  }
+
+  const audioResponse = await ai.convertToAudio(messageResponse.data)
+  if (audioResponse.error || !audioResponse.data) {
+    return Response.json(audioResponse, { status: 400 })
+  }
+
+  const uploadResult = await uploadFileToCloudinary({
+    data: audioResponse.data,
+    filename: `${slug}.mp3`,
+    folder: 'podcasts',
+  })
+
+
+  if (uploadResult.error || !uploadResult.data) {
+    return Response.json(uploadResult, { status: 400 })
+  }
+
+  const duration = getMP3Duration(audioResponse.data)
+  const createPodcast = await createNewPodcast({
+    name: message,
+    slug,
+    duration,
+    url: uploadResult.data.secure_url,
+    user: user._id.toString()
+  })
+
+    if (createPodcast.error) {
+        return Response.json(createPodcast, { status: 400 })
+    }
+
+    return Response.json({ message: 'Podcast created successfully', data: createPodcast.data.toJSON() }, { status: 201 })
+}
---
+++ src/app/api/utils/save_file.util.ts
@@ -0,0 +1,47 @@
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'
+import { Readable } from 'stream'
+import envs from '../envs'
+
+cloudinary.config({
+	cloud_name: envs.imageUpload.cloudName,
+	api_key: envs.imageUpload.apiKey,
+	api_secret: envs.imageUpload.apiSecret,
+})
+
+const upload = async (data: Buffer, filename: string, folder: string) => {
+    return new Promise<UploadApiResponse>((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream({
+            resource_type: 'auto',
+            type: 'upload',
+            filename_override: filename,
+            use_filename: true,
+            unique_filename: true,
+            folder,
+        }, (error, result) => {
+            if (result) {
+                resolve(result)
+            }
+            if (error) {
+                reject(error)
+            }
+        })
+
+        Readable.from(data).pipe(stream)
+    })
+}
+
+export const uploadFileToCloudinary = async ({
+    data, filename, folder
+}: {
+    data: Buffer, filename: string, folder: string
+}) => {
+    try {
+        const uplodResp = await upload(data, filename, folder)
+        return {
+            data: uplodResp
+        }
+    } catch (error) {
+        console.log('error saving file to cloudinary', error)
+        return { error: (error as Error).message }
+    }
+}
