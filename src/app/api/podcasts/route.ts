96b1bdc0c85d80d1ec2c8c8bdd9521ed6f2785f9
Babatunde Koiki <koikibabatunde14@gmail.com>
2024-04-17 20:57:26 +0100

    [feature]: update create podcasts serverless function max duration to 300s

--- src/app/api/podcasts/route.ts
+++ src/app/api/podcasts/route.ts
@@ -1,8 +1,6 @@
-import { MIN_S } from '../../../constants'
 import { requiresLogin } from '../middlewares/requires_login.middleware'
 import { createPodcast } from './create_podcast.route'
 import { getUserPodcasts } from './get_user_podcasts.route'
 
 export const POST = requiresLogin(createPodcast)
 export const GET = requiresLogin(getUserPodcasts)
-export const maxDuration = MIN_S * 5
