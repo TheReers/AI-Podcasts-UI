d3caec1f1569a6607a354b70c67a7618c772ec6f
Babatunde Koiki <koikibabatunde14@gmail.com>
2024-04-20 11:45:55 +0100

    [feature]: sort podcasts in recent to oldest

--- src/app/api/podcasts/get_user_podcasts.route.ts
+++ src/app/api/podcasts/get_user_podcasts.route.ts
@@ -6,9 +6,11 @@ export const getUserPodcasts: Handler = async (req) => {
     if (!user) {
         return Response.json({ message: 'Unauthorized' }, { status: 401 })
     }
-    
-    const podcasts = await podcastModel.find({ user: user._id })
-    
+
+    const podcasts = await podcastModel
+        .find({ user: user._id })
+        .sort({ updatedAt: -1, createdAt: -1 })
+
     return Response.json({
         data: podcasts.map((podcast) => podcast.toJSON()),
         message: 'Podcasts fetched successfully'
