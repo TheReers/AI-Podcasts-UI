2f8935429aff436804a566a975c2c2c21a177c08
adenikym <mm5adeniky@gmail.com>
2024-04-21 17:02:54 +0100

    feat:[audio player updated]

--- src/app/dashboard/components/AudioPlayer.tsx
+++ src/app/dashboard/components/AudioPlayer.tsx
@@ -2,24 +2,23 @@ import * as React from "react";
 import Player from "@madzadev/audio-player";
 import "@madzadev/audio-player/dist/index.css";
 
-   interface Props {
-    tracklist: string[];
-   }
+interface Props {
+  tracklist: {
+    url: string;
+    title: string;
+    tags: string[];
+  }[];
+}
 
-export default function AudioPlayer({
-tracklist 
-}: Props) {
-
-
- 
+export default function AudioPlayer({ tracklist }: Props) {
   return (
     <div className="">
       <Player
         trackList={tracklist}
-        includeTags={true}
-        includeSearch={true}
-        showPlaylist={true}
-        sortTracks={true}
+        includeTags={false}
+        includeSearch={false}
+        showPlaylist={false}
+        sortTracks={false}
         autoPlayNextTrack={true}
       />
     </div>
