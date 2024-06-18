3da8e18eedd2ccb50affcec617367a73d615683a
Maryam Mudasiru <mm5adeniky@gmail.com>
2024-06-18 16:42:13 +0100

    new tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,22 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
     extend: {
+      colors:{
+'alt-black':"#191919"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic-t": "url('../../public/gradient-background.png')",
+        "button-gradient":"linear-gradient(289.33deg, #D391E1 5.5%, #FFD9BB 89.93%)",
+        "box-gradient":"linear-gradient(289.33deg, #D391E1 5.5%, #FFD9BB 89.93%, rgba(255, 255, 255, 0.85) )",
+        "create-gradient":"linear-gradient(289.33deg, rgba(211, 145, 225, 0.07) 5.5%, rgba(255, 217, 187, 0.07) 89.93%)",
+        "br-pink":"rgba(224, 162, 219, 0.13)",
+        "t-grey":"rgba(135, 135, 135, 1)"
+        
       },
     },
   },
